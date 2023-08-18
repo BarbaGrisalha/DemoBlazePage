8e5520b9f84c6cec9f8d642227444f610987021d
Altamir <box.altamir@gmail.com>
2023-08-18 16:42:43 +0100

    Segunda Fase do Teste completo. Selecionar o aparelho celular desejado, inserido através de uma variável "celular". Passed Test.

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,25 @@
 package application;
 
+
+import jdk.jfr.Timespan;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class Program {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.demoblaze.com/");
+        String url = "https://www.demoblaze.com/";
+
+        driver.get(url);
         driver.manage().window().maximize();
 
-        //acedendo ao site .Screenshot para análise visual
+        //@Screenshot acedendo ao site
         File src01 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(src01, new File("/Users/altamir/Documents/scr01.png"));
@@ -31,7 +37,7 @@ public class Program {
         userName.sendKeys("admin");
         passWord.sendKeys("admin");
 
-        //Inserindo Login e Pass . Screenshot para análise visual
+        //@Screenshot Login e Pass
         File src02 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(src02, new File("/Users/altamir/Documents/scr02.png"));
@@ -45,17 +51,36 @@ public class Program {
         loginOK.click();
 
 
+        //Criando variável com nome do produto celular
+        String celular = "Iphone 6 32gb";
+
+        //Aguardando renderização para clicar
+        try {
+            Thread.sleep(TimeUnit.SECONDS.toMillis(4));
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement telemovel = driver.findElement(By.xpath("//a[text()='" + celular + "']"));
+        telemovel.click();
+         /*
+        ######################## ATÉ AQUI FUNCIONA BEM ########################
+         */
+        //*[@id='tbodyid']/div[2]/div/a
+
+        WebElement chooseTelemovel = driver.findElement(By.xpath("//*[@id='tbodyid']/div[2]/div/a"));
+        chooseTelemovel.click();
 
 
-
-        /*File src03 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        //@Screenshot selecção produto.
+        File src03 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
             FileUtils.copyFile(src03, new File("/Users/altamir/Documents/scr03.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }*/
+        }
 
-
-        // driver.close();
+        driver.close();
     }
 }
